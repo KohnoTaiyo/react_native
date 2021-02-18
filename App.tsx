17fcd17442a7b4e8beecb70c24d8d8a7f57fef25
KohnoTaiyo <taiyowork66@gmail.com>
2021-02-18 23:25:13 +0900

    finish add images

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+import logo from "./assets/logo.png";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.instructions}>
+        <Image source={logo} style={styles.logo} />
+        <Image
+          source={{ uri: "https://i.imgur.com/TkIrScD.png" }}
+          style={styles.logo}
+        />
+        Open up App.tsx to start working on your app!!!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +22,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10,
+  },
+  instructions: {
+    color: "#888",
+    fontSize: 18,
+    marginHorizontal: 15,
   },
 });
